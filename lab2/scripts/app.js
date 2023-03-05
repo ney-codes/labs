bcbf21a6ba83e8ce58bac05625df6a8b395bf4bc
Neema Mwansembo <neyharry2003@gmail.com>
2023-03-04 21:56:09 -0500

    Co-authored-by: Diego Diaz cruz <diego.diazcruz@dcmail.ca>

--- lab2/scripts/app.js
+++ lab2/scripts/app.js
@@ -2,59 +2,101 @@
 // Date Modified: March 1st, 2023
 // Description: This document contains all the functions for the website.
 
-(function () {
+// create a variable that contains the error message
+var errorMessage = document.getElementById("ErrorMessage");
 
-    function DisplayHome() {
-        $("#SubmitButton").on("click", function() {
-            location.href = 'login.html'
-        })
+function validateForm(){
 
-        $("main").addClass("container").append(`<p id="MainParagraph" class="mt-3 container">${ secondString }</p>`)
+    ContactFormValidate();
+
+    let firstname = document.forms["registerForm"]["firstName"].value;
+    let lastname = document.forms["registerForm"]["lastName"].value;
+    let email = document.forms["registerForm"]["emailAddress"].value;
+    let password = document.forms["registerForm"]["password"].value;
+    let confirmpassword = document.forms["registerForm"]["confirmPassword"].value;
+
+    if(firstname == ""){
+        errorMessage.textContent = "First Name must be filled out";
+        return false;
     }
-
-    function DisplayProducts() {
-        console.log("Products Page")
+    else if(lastname == ""){
+        errorMessage.textContent = "Last Name must be filled out";
+        return false;
     }
-
-    function DisplayServices() {
-        console.log("Services Page")
+    else if(email == ""){
+        errorMessage.textContent = "Email Address must be filled out";
+        return false;
     }
-
-
-
-
-    function DisplayContacts() {
-        console.log("Contact us Page")
-               
+    else if(password == ""){
+        errorMessage.textContent = "Password must be filled out";
+        return false;
     }
-
-
-
-    function DisplayReferences() {
-        console.log("References Page")
+    else if(confirmpassword == ""){
+        errorMessage.textContent = "You must confirm your password";
+        return false;
     }
-    
-    function Start() {
-        console.log("App Started Successfully!")
-
-        switch (document.title) {
-            case "Home - WEBD6201 Demo":
-                DisplayHome()
-                break
-            case "Products - WEBD6201 Demo":
-                DisplayProducts()
-                break
-            case "Services - WEBD6201 Demo":
-                DisplayServices()
-                break
-            case "Contact us - WEBD6201 Demo":
-                DisplayContacts()
-                break
-            case "Register - WEBD6201 Demo":
-                DisplayReferences()
-                break    
-        }       
+    else if(password != confirmpassword){
+        errorMessage.textContent = "Your password is not matching";
+        return false;
     }
+    else{
+        errorMessage.textContent = firstname;
+        return false;
+    }
+}
 
-    window.addEventListener("load", Start)
-})()
+    // select the input field by its ID
+    var firstName = $('#firstName');
+  
+    // bind the keyup event to the input field
+    firstName.on('keyup', function() {
+      // get the value of the input field
+      var firstName = $(this).val();
+  
+      // check if the length of the input value is less than 2
+      if (firstName.length < 2) {
+        // show an error message
+        $('#errorMessage').text('Input must be at least 2 characters long');
+      } else {
+        // clear the error message
+        $('#errorMessage').text('');
+      }
+    });
+
+  
+
+function AddContact(firstName, lastName, email) {
+    let contact = new core.Contact(firstName, lastName, email)
+    if (contact.serialize()) {
+        let key = contact.Name.substring(0, 1) + Date.now()
+        localStorage.setItem(key, contact.serialize())
+    }
+}
+
+function ValidateInput(inputFieldID, regularExpression, exception) {
+    let messageArea = $('#ErrorMessage').hide()
+
+    $('#' + inputFieldID).on("blur", function() {
+        let inputText = $(this).val()
+
+        if (!regularExpression.test(inputText)) {
+            // failure to match full name with regex
+
+            $(this).trigger("focus").trigger("select")
+
+            messageArea.addClass("alert alert-danger").text(exception).show()
+        } else {
+            // success in matching full name with regex
+
+            messageArea.removeAttr("class").hide()
+        }
+    })
+}
+
+function ContactFormValidate() {
+    let emailAddressPattern = /^[\w-\.]+@([\w-]+\.)+[\w-][\D]{2,10}$/g
+    let fullNamePattern = /^([A-Z][a-z]{1,25})((\s|,|-)([A-Z][a-z]{1,25}))*(\s|-|,)*([A-Z][a-z]{1,25})*$/g
+
+    ValidateInput("fullName", fullNamePattern, "Please enter a valid Full name which means a capitalized first name and capitalized last name")
+    ValidateInput("emailAddress", emailAddressPattern, "Please enter a valid Email Address")
+}
